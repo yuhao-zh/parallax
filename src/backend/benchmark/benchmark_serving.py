b8e0f35f4e2c534add75325e8df79d1e2ab7ed18
christ7 <31713990+christ-tt@users.noreply.github.com>
2025-09-21 06:12:10 -0700

    Default benchmark args (#80)

--- src/backend/benchmark/benchmark_serving.py
+++ src/backend/benchmark/benchmark_serving.py
@@ -1087,7 +1087,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--metric-percentiles",
         type=str,
-        default="96,97,98,99",
+        default="95,96,97,98,99,100",
         help="Comma-seperated list of percentiles for selected metrics. "
         'To report 25-th, 50-th, and 75-th percentiles, use "25,50,75". '
         'Default value is "99". '
