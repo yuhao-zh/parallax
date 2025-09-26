1c3232cd8d1b3f53b8a0930f88f3d63f18e179bc
TianyiZhao1437 <tim@gradient.network>
2025-09-26 13:09:42 +0800

    Get model info from huggingface config (#6)

--- src/backend/server/static_config.py
+++ src/backend/server/static_config.py
@@ -1,112 +1,8 @@
+import json
+
+from huggingface_hub import hf_hub_download
+
 from scheduling.model_info import ModelInfo
-
-MODEL_NAME_REMAP = {
-    "Qwen/Qwen3-0.6B-MLX-bf16": "Qwen/Qwen3-0.6B",
-    "Qwen/Qwen2.5-72B-Instruct": "Qwen/Qwen2.5-72B",
-}
-
-MODEL_INFO_MAP = {
-    "Qwen/Qwen3-0.6B": ModelInfo(
-        model_name="Qwen/Qwen3-0.6B-bf16",
-        head_size=128,
-        hidden_dim=1024,
-        intermediate_dim=3072,
-        num_attention_heads=16,
-        num_kv_heads=8,
-        vocab_size=151936,
-        num_layers=28,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen3-8B": ModelInfo(
-        model_name="Qwen/Qwen3-8B-bf16",
-        head_size=128,
-        hidden_dim=4096,
-        intermediate_dim=12288,
-        num_attention_heads=32,
-        num_kv_heads=8,
-        vocab_size=151936,
-        num_layers=36,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen3-32B": ModelInfo(
-        model_name="Qwen/Qwen3-32B-bf16",
-        head_size=128,
-        hidden_dim=5120,
-        intermediate_dim=25600,
-        num_attention_heads=64,
-        num_kv_heads=8,
-        vocab_size=151936,
-        num_layers=64,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen3-32B-FP8": ModelInfo(
-        model_name="Qwen/Qwen3-32B-fp8",
-        head_size=128,
-        hidden_dim=5120,
-        intermediate_dim=25600,
-        num_attention_heads=64,
-        num_kv_heads=8,
-        vocab_size=151936,
-        num_layers=64,
-        ffn_num_projections=3,
-        param_bytes_per_element=1,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen3-14B": ModelInfo(
-        model_name="Qwen/Qwen3-14B",
-        head_size=128,
-        hidden_dim=5120,
-        intermediate_dim=25600,
-        num_attention_heads=40,
-        num_kv_heads=8,
-        vocab_size=151936,
-        num_layers=40,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen2.5-72B": ModelInfo(
-        model_name="Qwen/Qwen2.5-72B-bf16",
-        head_size=128,
-        hidden_dim=8192,
-        intermediate_dim=29568,
-        num_attention_heads=64,
-        num_kv_heads=8,
-        vocab_size=152064,
-        num_layers=80,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-    ),
-    "Qwen/Qwen3-235B-A22B": ModelInfo(
-        model_name="Qwen/Qwen3-235B-A22B-bf16",
-        head_size=128,
-        hidden_dim=4096,
-        intermediate_dim=12288,
-        num_attention_heads=64,
-        num_kv_heads=4,
-        vocab_size=151936,
-        num_layers=94,
-        ffn_num_projections=3,
-        param_bytes_per_element=2,
-        cache_bytes_per_element=2,
-        embedding_bytes_per_element=2,
-        num_local_experts=128,
-        num_experts_per_tok=8,
-    ),
-}
 
 # Supported model list
 MODEL_LIST = [
@@ -133,9 +29,41 @@ NODE_JOIN_COMMAND_PUBLIC_NETWORK = """python src/parallax/launch.py \
 
 
 def get_model_info(model_name):
-    if model_name not in MODEL_INFO_MAP:
-        model_name = MODEL_NAME_REMAP.get(model_name, model_name)
-    return MODEL_INFO_MAP.get(model_name, None)
+    config_path = hf_hub_download(repo_id=model_name, filename="config.json")
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        f.close()
+
+    # get quant method
+    quant_method = config.get("quant_method", None)
+    quantization_config = config.get("quantization_config", None)
+    if quant_method is None and quantization_config is not None:
+        quant_method = quantization_config.get("quant_method", None)
+
+    if quant_method is None:
+        param_bytes_per_element = 2
+    elif quant_method == "fp8":
+        param_bytes_per_element = 1
+    elif quant_method in ("mxfp4", "int4", "awq", "gptq"):
+        param_bytes_per_element = 0.5
+
+    model_info = ModelInfo(
+        model_name=model_name,
+        head_size=config.get("head_dim", 128),
+        hidden_dim=config.get("hidden_size", 0),
+        intermediate_dim=config.get("intermediate_size", 0),
+        num_attention_heads=config.get("num_attention_heads", 0),
+        num_kv_heads=config.get("num_key_value_heads", 0),
+        vocab_size=config.get("vocab_size", 0),
+        num_layers=config.get("num_hidden_layers", 0),
+        ffn_num_projections=3,
+        param_bytes_per_element=param_bytes_per_element,
+        cache_bytes_per_element=2,
+        embedding_bytes_per_element=2,
+        num_local_experts=config.get("num_experts", None),
+        num_experts_per_tok=config.get("num_experts_per_tok", None),
+    )
+    return model_info
 
 
 def get_model_list():
