910a4b70df473d0553e5163bc6adbfd4388158e2
Lonely Guy <65459494+LonelyGuy-SE1@users.noreply.github.com>
2026-01-26 11:20:16 +0530

    docs(readme+static): update static+readme with glm 4.7 flash and add parallax x account into readme (#389)

--- src/backend/server/static_config.py
+++ src/backend/server/static_config.py
@@ -27,12 +27,13 @@ MODELS = {
     "openai/gpt-oss-safeguard-20b": "lmstudio-community/gpt-oss-safeguard-20b-MLX-MXFP4",
     "openai/gpt-oss-safeguard-120b": "lmstudio-community/gpt-oss-safeguard-120b-MLX-MXFP4",
     # zai-org GLM4 Models
-    "zai-org/GLM-4.7": "mlx-community/GLM-4.7-4bit",
     "zai-org/GLM-4.6": "mlx-community/GLM-4.6-4bit",
     "zai-org/GLM-4.6-FP8": "mlx-community/GLM-4.6-4bit",
     "zai-org/GLM-4.5-Air": "lmstudio-community/GLM-4.5-Air-MLX-8bit",
     "zai-org/GLM-4.7": "mlx-community/GLM-4.7-4bit",
-    # Other Models
+    "zai-org/GLM-4.7-Flash": "mlx-community/GLM-4.7-Flash-4bit",
+    "zai-org/GLM-4.7-Flash-FP8": "mlx-community/GLM-4.7-Flash-8bit",
+    # Minimax M2 Models
     "MiniMaxAI/MiniMax-M2.1": "mlx-community/MiniMax-M2.1-4bit",
     "MiniMaxAI/MiniMax-M2": "mlx-community/MiniMax-M2-4bit",
     # ======================================= End ========================================#
